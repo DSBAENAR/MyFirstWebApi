221106d9145caec587f2bc835511685a05692296
David Salomón Baena Rubio <68250833+DSBAENAR@users.noreply.github.com>
2023-10-06 13:28:24 -0500

    Add files via upload

---
+++ MyFirstWebAPI/MyFirstWebAPI/Controllers/AutoresControllers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebAPI.Entidades;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresControllers : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() {
+                new Autor() { Id = 1, Name = "Salomon" },
+                new Autor() { Id = 2, Name = "Alvaro" }
+                };
+        }
+    }
+}
